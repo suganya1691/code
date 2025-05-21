f16a9d4902c5620703c5593cb0dccfd4daff9981
Suganya <gs.suganya16@gmail.com>
2025-05-21 17:30:04 +1000

    bugFix : Fix loading icon component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SearchBar from './components/SearchBar';
 import EmployeeDetails from './components/EmployeeDetails';
 import CompanyDetails from './components/CompanyDetails';
 import Pagination from './components/Pagination';
+import LoadingSpinner from './components/LoadingSpinner';
 import { getSelectedEmployee,getPaginated } from './redux/selectors/employeeSelectors';
 import {ErrorBoundary} from 'react-error-boundary';
 
@@ -39,11 +40,7 @@ function App() {
       <div className='px-4 py-4 flex justify-end'>
         <SearchBar  searchText={searchText} />
       </div> 
-      {loading &&  
-        <div className="max-w-md mx-auto bg-white border border-blue-300 text-blue-700 p-4 rounded-lg shadow-md mt-4" role="alert">
-         <h2 className="text-lg font-semibold mb-2">Info</h2>
-         <p>Fetching Data from server</p> 
-        </div>}
+      {loading &&  <LoadingSpinner />}
       {!loading && processedEmployees && (<EmployeeList employees={processedEmployees}/>) }
       {error && 
         <div className="max-w-md mx-auto bg-white border border-red-300 text-red-700 p-4 rounded-lg shadow-md mt-4" role="alert">
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoadingSpinner() {
+  return (
+     <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
